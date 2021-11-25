850b138ae2791dd5e403320c08978e5ff127742a
Doofster <dooferall09@gmail.com>
2021-11-25 19:11:39 +0530

    Added Vanilla CSS Variables, Icons, and reworked Mobile UI.

--- sass-tuts/index.php
+++ sass-tuts/index.php
@@ -1,38 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="./resources/css/main.css">
-	<title>SASS Tutorial</title>
-</head>
-<body>
-	<div>
-		<div class="headerWrapper">
-			<header>
-				<img class="fit" src="https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg"/>
-				<h1>Tutorial</h1>
-			</header>
-		</div>
-		<div class="navWrapper">
-			<nav>
-				<a class="navItem" href="#">Link 01</a>
-				<a class="navItem" href="#">Link 02</a>
-				<a class="navItem" class="navItem" href="#">Link 03</a>
-				<a class="navItem" class="navItem" class="navItem" href="#">Link 04</a>
-			</nav>
-		</div>
-		<div class="mainWrapper">
-			<div class="mainLeftSidebar">Sidebar Content Placeholder</div>
-			<div class="mainMiddleContent">Main Content Placeholder</div>
-			<div class="mainRightSidebar">Sidebar Content Placeholder</div>
-		</div>
-		<div class="footerWrapper">
-			<footer>
-				<h4>Property of Brainstorm Force | Created by Zane Vaz</h4>
-			</footer>
-		</div>
-	</div>
-	<script type="text/javascript" src="./resources/js/main.js"></script>
-</body>
-</html>
+<?php include './modules/base/header.php'?>
+<div class="mainWrapper">
+	<div class="mainLeftSidebar">Sidebar Content Placeholder</div>
+	<div class="mainMiddleContent">Main Content Placeholder</div>
+	<div class="mainRightSidebar">Sidebar Content Placeholder</div>
+</div>
+<?php include './modules/base/footer.php'?>
---
+++ sass-tuts/modules/base/footer.php
@@ -0,0 +1,8 @@
+		<div class="footerWrapper">
+			<footer>
+				<h4>Property of Brainstorm Force | Created by Zane Vaz</h4>
+			</footer>
+		</div>
+		<script type="text/javascript" src="./resources/js/main.js"></script>
+	</body>
+</html>
