be809aaab0f37b8c701855704bb3aeb95e2af49b
Sushma Kure <sushmak@bsf.io>
2021-11-26 17:02:11 +0530

    changes made to index file

--- sass-tuts/index.php
+++ sass-tuts/index.php
@@ -1,6 +1,6 @@
 <?php include './modules/base/header.php'?>
 <div class="mainWrapper">
-	<div class="mainLeftSidebar">Sidebar Content Placeholder</div>
+	<div class="mainLeftSidebar">Sidebar Content Placeholder to this line</div>
 	<div class="mainMiddleContent">Main Content Placeholder</div>
 	<div class="mainRightSidebar">Sidebar Content Placeholder Released</div>
 </div>
