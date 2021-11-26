688af22d74bf766e28ff5c30f690f8d5128156c7
Sushma Kure <sushmak@bsf.io>
2021-11-26 16:53:27 +0530

    Changed class added

--- sass-tuts/index.php
+++ sass-tuts/index.php
@@ -1,5 +1,5 @@
 <?php include './modules/base/header.php'?>
-<div class="mainWrapper body-template">
+<div class="mainWrapper body-template changed">
 	<div class="mainLeftSidebar">Sidebar Content Placeholder Example</div>
 	<div class="mainMiddleContent">Main Content Placeholder</div>
 	<div class="mainRightSidebar">Sidebar Content Placeholder</div>
