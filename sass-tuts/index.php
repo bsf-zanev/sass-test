0308737f56b91c61e58875a84d826ed6647c986d
Zane Vaz <zanev@bsf.io>
2021-11-30 12:37:58 +0530

    Fixed Entire Project Structure.

--- sass-tuts/index.php
+++
@@ -1,7 +0,0 @@
-<?php include './modules/header.php'?>
-<div class="mainWrapper">
-	<div class="mainLeftSidebar">Sidebar Content Placeholder to this line</div>
-	<div class="mainMiddleContent">Main Content Placeholder</div>
-	<div class="mainRightSidebar">Sidebar Content Placeholder Released</div>
-</div>
-<?php include './modules/footer.php'?>
